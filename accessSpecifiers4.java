a06f0b528a0735ac540e1c19205b335a1101d0ac
pallu-123 <82052938+pallu-123@users.noreply.github.com>
2022-01-17 00:04:13 +0530

    Add files via upload

---
+++ accessSpecifiers4.java
@@ -0,0 +1,16 @@
+package pack2;
+import pack1.*;
+
+
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
+
+
+	}
+
+}
