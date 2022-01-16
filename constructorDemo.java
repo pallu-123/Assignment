a6a514a62a3901039a3a2b141e8944523e2c3756
pallu-123 <82052938+pallu-123@users.noreply.github.com>
2022-01-16 23:56:37 +0530

    Add files via upload

---
+++ constructorDemo.java
@@ -0,0 +1,42 @@
+package constructor;
+
+//default constructor
+class EmpInfo{
+	int id;
+	String name;
+
+void display() {
+	System.out.println(id+" "+name);
+	}
+}
+
+
+public class constructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+
+		emp1.display();
+		emp2.display();
+		}
+	}
+
+	//parameterized constructor
+	class Std{
+		int id;
+		String name;
+
+		Std(int i,String n)
+		{
+		id=i;
+		name=n;
+		}
+
+		void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
