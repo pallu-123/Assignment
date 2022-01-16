60934d97de838783fba77689a2b185e8960670dd
pallu-123 <82052938+pallu-123@users.noreply.github.com>
2022-01-16 23:55:35 +0530

    Add files via upload

---
+++ innerClassAssisted3.java
@@ -0,0 +1,21 @@
+package innerClass;
+
+//anonymous inner class
+abstract class AnonymousInnerClass {
+	   public abstract void display();
+	}
+
+
+public class innerClassAssisted3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnonymousInnerClass i = new AnonymousInnerClass() {
+
+	         public void display() {
+	            System.out.println("Anonymous Inner Class");
+	         }
+	      };
+	      i.display();
+	   }
+	}
